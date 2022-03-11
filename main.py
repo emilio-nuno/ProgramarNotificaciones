6b3f08b549b1a2423f122d0a804914dac3b92747
emilio-nuno <emilio.nuno@alumnos.udg.mx>
2022-03-11 14:07:57 -0600

    Se genera un datetime basado en horal local

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import datetime
 
+traducciones = {
+    'Monday': 'L',
+    'Tuesday': 'M',
+    'Wednesday': 'I',
+    'Thursday': 'J',
+    'Friday': 'V',
+    'Saturday': 'S',
+    'Sunday': 'D',
+}
+
+datos_prueba = {
+    'L': {'5:00': [1,2],'4:00': [3,4], '8:00': [5,6]}
+}
+
 def consultar_usuarios(fecha_actual):
     """Esta función nos permitirá consultar la base de datos de usuarios"""
+    dia = fecha_actual.strftime("%A")
+    hora = fecha_actual.hour
+    if traducciones[dia] in datos_prueba:
+        print(datos_prueba[traducciones[dia]])
+    else:
+        print("Nada programado para hoy")
     pass
 
 def main():
     """Esta función checará si existe alguna notificación que se tiene que mandar"""
-    print("Hola mundo2")
+    consultar_usuarios(datetime.datetime.now())
 
 if __name__ == "__main__":
     main()
