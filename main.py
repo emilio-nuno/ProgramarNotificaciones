bc3153715436da5a5db1be8bdd1c03ea8a5b98d6
emilio-nuno <emilio.nuno@alumnos.udg.mx>
2022-04-07 12:24:11 -0500

    Agregada búsqueda de programas en hora actual + 1

--- main.py
+++ main.py
@@ -17,25 +17,24 @@ traducciones = {
 
 def enviar_notificacion(codigo_usuario, nombre_programa, horario):
     """Esta función se encargará de enviar la notificación al usuario"""
+
     url = "https://onesignal.com/api/v1/notifications"
     carga = {
 	"app_id": "51c63cff-c83b-405e-927b-a9ce3234ec65",
 	"include_player_ids": [codigo_usuario],
-	"contents": {"en": f"The program {nombre_programa} starts at {horario}", "es": f"El programa {nombre_programa} comienza a la{'s' if horario != '01:00' else ''} {horario}"},
+	"contents": {"en": f"The program {nombre_programa} will start in an hour", "es": f"El programa {nombre_programa} comenzará en una hora"},
 	"headings": {"en": "Program Notification", "es": "Notificación de Programa"}
     }
     respuesta = requests.post(url, headers={"Content-Type": "application/json; charset=utf-8", "Authorization": "Basic YTQzM2RiOTQtYzUxYy00MTlmLWE3NTQtODhiYWJiYmFhYTBm"}, json=carga)
 
 def consultar_notificaciones(fecha_actual, cnx):
     """Esta función checará si existe alguna notificación que se tiene que mandar"""
-    #TODO: Hacer que el comando seleccione los programas que empiezan en la siguiente hora
-    #TODO: Checar si solo vamos a generar una notificación para una hora antes
     
     comando = ("SELECT codigo_usuario, nombre_programa, horario FROM radiocucei.notificaciones WHERE horario = %s AND dia = %s")
     cursor = cnx.cursor()
     dia = traducciones[fecha_actual.strftime("%A")]
-    hora = fecha_actual.hour
-    valores = (f"{hora}:00", dia)
+    hora = (fecha_actual + datetime.timedelta(hours=1)).hour
+    valores = (f"{'0' if hora < 10 else ''}{hora}:00", dia)
     cursor.execute(comando, valores)
     for (codigo, nombre_programa, horario) in cursor:
         enviar_notificacion(codigo, nombre_programa, horario)
